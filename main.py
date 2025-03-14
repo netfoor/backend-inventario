aa023d11fc1ea9b4aa46c332b9949469d4534e74
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2025-03-14 11:11:18 -0600

    fix: Fix middleware CORS

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
-from app.routes import persona, ubicacion, asignacion, inventario, articulo
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import ubicacion, persona, articulo, inventario, asignacion
 
 app = FastAPI()
 
-# Incluir las rutas
-app.include_router(persona.router, prefix="/personas", tags=["Personas"])
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En producción, especifica los orígenes permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Incluye tus rutas
 app.include_router(ubicacion.router, prefix="/ubicaciones", tags=["Ubicaciones"])
-app.include_router(asignacion.router, prefix="/asignaciones", tags=["Asignaciones"])
-app.include_router(inventario.router, prefix="/inventarios", tags=["Inventarios"])
+app.include_router(persona.router, prefix="/personas", tags=["Personas"])
 app.include_router(articulo.router, prefix="/articulos", tags=["Articulos"])
+app.include_router(inventario.router, prefix="/inventarios", tags=["Inventarios"])
+app.include_router(asignacion.router, prefix="/asignaciones", tags=["Asignaciones"])
 
 @app.get("/")
 def read_root():
